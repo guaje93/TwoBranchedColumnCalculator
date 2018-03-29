a5216a013696f8739ffa4349873239a922ecca3f
guaje93 <jakub.szymczak93@gmail.com>
2018-03-29 14:04:35 +0200

    version 2.01

--- src/main/java/database/DataBaseBuilder.java
+++ src/main/java/database/DataBaseBuilder.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class DataBaseBuilder {
 
 	public static final String DRIVER = "org.sqlite.JDBC";
-	public static final String DB_URL = "jdbc:sqlite:src/main/resources/sections.db";
+	public static final String DB_URL = "jdbc:sqlite:src/main/resources/database/sections.db";
 
 	private Connection conn;
 	private Statement stat;
